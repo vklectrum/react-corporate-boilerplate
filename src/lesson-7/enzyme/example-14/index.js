3c60bf6126a11b2614a2231931ec41d0b65dc1a3
d.vakatsiienko <d.vakatsiienko@lectrum.io>
2018-01-08 13:14:41 +0200

    add lesson7/example-14

---
+++ src/lesson-7/enzyme/example-14/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Example extends Component {
+    render () {
+        return (
+            <section>
+                <h1 title = 'Greeting message.'>Welcome!</h1>
+                <p className = 'paragraph-main'>Lorem ipusm dolor sit amet!</p>
+            </section>
+        );
+    }
+}
